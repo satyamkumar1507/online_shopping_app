d03ed8471f5657edbe4a2bdd89b2761465b42121
satyam kumar <167162735+satyamkumar1507@users.noreply.github.com>
2025-08-04 16:37:02 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
   set -e
 
   # Pull the latest Docker image from Docker Hub
-  docker pull trainwithshubham/tws-online-shop-app-demo:latest
+  docker pull satyamkumar1507/tws-online-shop-app-demo:latest
 
   # Run the Docker image as a container
-  docker run -d -p 5173:5173 trainwithshubham/tws-online-shop-app-demo:latest
+  docker run -d -p 5173:5173 satyamkumar1507/tws-online-shop-app-demo:latest
